0dc64d7eab7a1100aed8dc691d4b0ef928ee8576
Oscar Recio <reciog.oscar@hotmail.com>
2024-03-05 17:41:44 -0600

    basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav-bar />
+  <div class="mx-auto mt-12 container">
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex justify-end mb-8">
+    <button class="btn btn-accent btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+    </button>
+  </div>
+  <div class="space-y-4">
+    <div class="card lg:card-side bg-base-100 shadow-lg" v-for="index in 100" :key="index">
+      <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+      <div class="card-body">
+        <h2 class="card-title">New album is released!</h2>
+        <p>Click the button to listen on Spotiwhy app.</p>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary">Listen</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
